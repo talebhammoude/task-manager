0c4bdd562880f8d02854ccc33aea5b1884a15958
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-07-01 00:34:23 +0200

    added example login

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import SignIn from './Components/Login/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      <SignIn/>
       </header>
     </div>
   );
